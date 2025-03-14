44b3c562a0f7e40ce293d9bbea6843b605ab7ebf
Omer Muhammed Gundogdu <o.muhammed.gundogdu@gmail.com>
2025-03-14 09:11:15 +0300

    UI testleri için CI/CD pipeline ayarlamaları ve bazı iyileştirmeler

--- ui-tests/src/test/java/test1/loginTest.java
+++ ui-tests/src/test/java/test1/loginTest.java
@@ -33,13 +33,14 @@ public class loginTest {
 
 
     @Test
-    public void testValidLogin(){
+    public void testValidLogin() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("muhammed.gundogdu.test@gmail.com"); // email
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Test_2905"); //şifre
         driver.findElement(By.xpath("//input[@name='rememberme']")).click(); //Beni Hatırla
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase basis-1/2']")).click(); //Giriş Butonu
+        Thread.sleep(5000);
 
        WebElement profileElement = driver.findElement(By.xpath("//span[@class='md:hidden lg:inline' and text()='Hesabım']"));
        Assert.assertTrue(profileElement.isDisplayed(), "Kullanıcı başarılı giriş yaptı!");
@@ -47,13 +48,13 @@ public class loginTest {
 
 
     @Test
-    public void testInvalidLogin(){
+    public void testInvalidLogin() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@gmail.com"); // email
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Test_2905"); //şifre
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase basis-1/2']")).click(); //Giriş Butonu
-
+        Thread.sleep(5000);
 
        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='p-1 text-center']"));
        Assert.assertTrue(errorMessage.isDisplayed(), "Hata mesajı görüntülendi!");
@@ -61,25 +62,27 @@ public class loginTest {
 
 
     @Test
-    public void testEmptyFields(){
+    public void testEmptyFields() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys(""); // email
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys(""); //şifre
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase basis-1/2']")).click(); //Giriş Butonu
+        Thread.sleep(5000);
 
         WebElement validationMessage = driver.findElement(By.xpath("//span[@class='label-text-alt text-error' and text()='Bu alan zorunludur'][1]"));
         Assert.assertTrue(validationMessage.isDisplayed(), "Boş alanlar için validasyon çalışmıyor!");
     }
 
     @Test
-    public void testForgotPasswordEmail(){
+    public void testForgotPasswordEmail() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//label[@class='btn btn-ghost min-h-6 w-fit min-h-6 h-fit p-0' and text()='Şifremi Unuttum']")).click(); //Şifremi unuttum
         driver.findElement(By.xpath("//input[@value='email']")).click(); //E-Mail'e Gönder Seçeneği
         driver.findElement(By.xpath("//input[@placeholder='E-Posta']")).sendKeys("muhammed.gundogdu.test@gmail.com"); // email
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase' and text()='Gönder']")).click(); //Şifre Yenileme Butonu
+        Thread.sleep(5000);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='login-tab' and text()='Giriş Yap']")));
@@ -90,13 +93,14 @@ public class loginTest {
 
 
     @Test
-    public void testForgotPasswordFalseEmail(){
+    public void testForgotPasswordFalseEmail() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//label[@class='btn btn-ghost min-h-6 w-fit min-h-6 h-fit p-0' and text()='Şifremi Unuttum']")).click(); //Şifremi unuttum
         driver.findElement(By.xpath("//input[@value='email']")).click(); //E-Mail'e Gönder Seçeneği
         driver.findElement(By.xpath("//input[@placeholder='E-Posta']")).sendKeys("test@gmail.com"); // email
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase' and text()='Gönder']")).click(); //Şifre Yenileme Butonu
+        Thread.sleep(5000);
 
         WebElement successMessage = driver.findElement(By.xpath("//div[@class='p-1 block text-center' and text()='Kullanıcı bulunamadı']"));
         Assert.assertTrue(successMessage.isDisplayed(),"Bu E-Mail'e ait bir kullanıcı bulunamadı");
@@ -104,13 +108,14 @@ public class loginTest {
 
 
     @Test
-    public void testForgotPasswordPhone(){
+    public void testForgotPasswordPhone() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//label[@class='btn btn-ghost min-h-6 w-fit min-h-6 h-fit p-0' and text()='Şifremi Unuttum']")).click(); //Şifremi unuttum
         driver.findElement(By.xpath("//input[@value='phone']")).click(); //Telefon'a Gönder Seçeneği
         driver.findElement(By.xpath("//input[@placeholder='Telefon']")).sendKeys("5538011753"); // phone
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase' and text()='Gönder']")).click(); //Şifre Yenileme Butonu
+        Thread.sleep(5000);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='login-tab' and text()='Giriş Yap']")));
@@ -121,13 +126,14 @@ public class loginTest {
 
 
     @Test
-    public void testForgotPasswordFalsePhone(){
+    public void testForgotPasswordFalsePhone() throws InterruptedException {
         driver.findElement(By.xpath("//span[@class='md:hidden lg:inline whitespace-nowrap']")).click(); // Giriş Yap
         driver.findElement(By.xpath("//a[@id='login-tab']")).click(); // Login tab
         driver.findElement(By.xpath("//label[@class='btn btn-ghost min-h-6 w-fit min-h-6 h-fit p-0' and text()='Şifremi Unuttum']")).click(); //Şifremi unuttum
         driver.findElement(By.xpath("//input[@value='phone']")).click(); //Telefon'a Gönder Seçeneği
         driver.findElement(By.xpath("//input[@placeholder='Telefon']")).sendKeys("5538010000"); // phone
         driver.findElement(By.xpath("//button[@class='btn btn-secil mx-auto w-fit uppercase' and text()='Gönder']")).click(); //Şifre Yenileme Butonu
+        Thread.sleep(5000);
 
         WebElement successMessage = driver.findElement(By.xpath("//div[@class='p-1 block text-center' and text()='Kullanıcı bulunamadı']"));
         Assert.assertTrue(successMessage.isDisplayed(),"Bu Numaraya ait bir kullanıcı bulunamadı");
